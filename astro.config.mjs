244ed19db7f0ec611e5f9312ca55e8d204106649
Stimsims <stimsims@learning.energy>
2024-10-31 11:16:58 +1100

    added base url

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,13 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import svelte from '@astrojs/svelte';
 
+// Load environment variables
+import dotenv from 'dotenv';
+dotenv.config();
+
 export default defineConfig({
     site: 'https://Stimsims.github.io', // Full URL of your GitHub Pages site
-    base: '/stimsims-website-2', // Base path for your site
+    base: process.env.ASTRO_BASE_URL || '/', // Base path for your site
     integrations: [mdx(), svelte({ preprocess: [] })],
     
 });
