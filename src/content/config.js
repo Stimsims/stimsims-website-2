b82a338abb6d8928cf007e73d31a285da4a89999
Stimsims <stimsims@learning.energy>
2025-01-16 21:01:59 +1100

    correct submodule deploy error from stimsims-astro directory

--- src/content/config.js
+++ src/content/config.js
@@ -1,17 +1,47 @@
 import { defineCollection, z } from 'astro:content';
+import { getImage } from "astro:assets";
 
 const postCollection = defineCollection({
+  integrations: [
+    // Enable image optimization
+    'astro:assets',
+  ],
   schema: ({ image }) => z.object({
     title: z.string(),
     description: z.string(),
     pubDate: z.string(),
     author: z.string(),
-    cover: image().refine((img) => img.width >= 300, {
-      message: "Cover image must be at least 300 pixels wide!",
-    }),
+    // cover: z.string(),
+    // twitterImage: z.string(),
+    cover: image(),
+    twitterImage: image(),
+    // twitterImage: image()
+    // .transform((src) => {
+    //   console.log("twitterImage src", src)
+    //   const optimizedImage = getImage({
+    //     src, // Path to the image
+    //     width: 800, // Desired width
+    //     height: 418, // Desired height
+    //     format: "webp", // Modern image format for optimization
+    //   }).catch(e => {
+    //     console.error("twitterImage transformer error", e)
+    //   });
+    //   return optimizedImage;
+    // }),
     coverAlt: z.string(),
     tags: z.array(z.string()),
-    published: z.boolean()
+    published: z.boolean(),
+    // images: z.array(image().transform((src) => {
+    //   console.log("post image transformer src", src)
+    //   const optimizedImage = getImage({
+    //     src, // Path to the image
+    //     format: "webp", // Modern image format for optimization
+    //   }).catch(e => {
+    //     console.error("post image transformer error", e)
+    //   });
+    //   console.log("post image optimisedImage", optimizedImage)
+    //   return optimizedImage;
+    // }))
   }),
 });
 
